a56f4d13e34f7735a402eb6f2ba76d0ba2f04c70
Mathius Johnson <devachievem@gmail.com>
2020-09-15 23:41:03 +0000

    admin dashboard displaying counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
   def show
+    @category = Category.find(params[:id])
+    @products = @category.products.order(created_at: :desc)
+    # @category = Category.all
   end
 end
