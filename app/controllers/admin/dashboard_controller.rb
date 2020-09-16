8d6e922124c57c24ea1cd219b6efe6a73fd69e98
Mathius Johnson <devachievem@gmail.com>
2020-09-16 17:05:04 +0000

    completed content for category page, wth ability to create new categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,8 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
   def show
-    @category = Category.find(params[:id])
-    @products = @category.products.order(created_at: :desc)
+    @categories = Category.order(id: :desc).all
+    @products = Product.order(id: :desc).all
     # @category = Category.all
   end
 end
