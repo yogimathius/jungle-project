6a632588d5c03be83e8f81d408d91cb4ea7c3116
Mathius Johnson <devachievem@gmail.com>
2020-09-22 04:11:35 +0000

    add to cart spec passing

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+    within('.product', :match => :first) do
+      click_link_or_button('Add')
+    end
+    # DEBUG
+    puts page.html
+    save_screenshot
+
+    # VERIFY
+    expect(page).to have_content 'My Cart (1)'
+  end
+end
